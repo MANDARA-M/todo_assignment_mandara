7440586986c137df8a51154a1c0dd1d335ee2ebd
MANDARA-M <mandaramjncs@gmail.com>
2025-11-30 15:13:37 +0530

    Task GridView for different screen sizes added

--- lib/Screens/todo/todo_list_screen.dart
+++ lib/Screens/todo/todo_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:Task_ToDo/Screens/todo/todo_card_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 import '../../enums/app_enums.dart';
 import '../../firebase/services/data_service.dart';
@@ -21,6 +22,8 @@ class TodoListScreenState extends State<TodoListScreen> {
   final _dataService = DataService.instance;
 
   TodoScreenType get todoScreenType => widget.todoScreenType;
+
+  List<QueryDocumentSnapshot<Task>> todos = [];
 
   Stream<QuerySnapshot<Task>> getStream() {
     Stream<QuerySnapshot<Task>> _stream;
@@ -72,27 +75,29 @@ class TodoListScreenState extends State<TodoListScreen> {
           return Center(child: const CircularProgressIndicator());
         }
 
-        final todos = snapshot.requireData.docs;
+        todos.clear();
+        todos.addAll(snapshot.requireData.docs);
+
         if (todos.isEmpty) {
           return const Center(
             child: Text('No Todos Added Yet!', style: TextStyle(color: Colors.white)),
           );
         }
 
-        return _listOfTodos(todos);
+        return _listOfTodos;
       },
     );
   }
 
-  Widget _listOfTodos(List<QueryDocumentSnapshot<Task>> todos) {
+  Widget get _listOfTodos {
     final child = LayoutBuilder(
       builder: (context, constraints) {
         if (constraints.maxWidth < 600) {
-          // Mobile layout
-          return _mobileLayout(todos);
+          return _mobileLayout;
+        } else if (constraints.maxWidth < 1000) {
+          return _tabletScreenLayout;
         } else {
-          // Bigger screen layout
-          return _biggerScreenLayout(todos);
+          return _biggerScreenLayout;
         }
       },
     );
@@ -100,29 +105,24 @@ class TodoListScreenState extends State<TodoListScreen> {
     return Padding(padding: const EdgeInsets.all(12.0), child: child);
   }
 
-  Widget _mobileLayout(List<QueryDocumentSnapshot<Task>> todos) {
-    return ListView.separated(
-      separatorBuilder: (context, index) => SizedBox(height: 8),
+  Widget get _mobileLayout => ListView.separated(separatorBuilder: (context, index) => SizedBox(height: 8), itemCount: todos.length, itemBuilder: _itemBuilder);
+
+  Widget get _tabletScreenLayout => _gridLayout(crossAxisCount: 2);
+
+  Widget get _biggerScreenLayout => _gridLayout(crossAxisCount: 3);
+
+  Widget _gridLayout({required int crossAxisCount}) {
+    return MasonryGridView.count(
       itemCount: todos.length,
-      itemBuilder: (context, index) {
-        final document = todos[index];
-        return _taskWidget(document: document);
-      },
+      crossAxisCount: crossAxisCount,
+      mainAxisSpacing: 8.0,
+      crossAxisSpacing: 8.0,
+      itemBuilder: _itemBuilder,
     );
   }
 
-  Widget _biggerScreenLayout(List<QueryDocumentSnapshot<Task>> todos) {
-    return GridView.builder(
-      itemCount: todos.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1.0, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0),
-      itemBuilder: (BuildContext context, int index) {
-        final document = todos[index];
-        return _taskWidget(document: document);
-      },
-    );
-  }
-
-  Widget _taskWidget({required QueryDocumentSnapshot<Task> document}) {
+  Widget _itemBuilder(BuildContext context, int index) {
+    final document = todos[index];
     final docId = document.id;
     final task = document.data();
 
